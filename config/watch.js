68775bcceaaf296ac24fa9a8f552546c9f59284f
Matthew Bishop <matt.bis.pb@gmail.com>
2017-05-15 17:43:24 +0100

    rem: watch

--- config/watch.js
+++
@@ -1,6 +0,0 @@
-const util = require('util')
-const memwatch = require('memwatch-next')
-//const log = require('./log')
-memwatch.on('leak', function(info) {
-  console.info(`MEMWATCH:info:${util.inspect(info, {depth: null})}`)
-})
