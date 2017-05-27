709fa132e8d717830293d3517c3c4cfd9b617b79
Matthew Bishop <mattbis@users.noreply.github.com>
2017-05-27 02:27:08 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
 module.exports = function() {
-//const args = require('./config/args')
-//const paths = require('./config/paths')
-//const watch = require('./config/watch')
-//const constants = require('./config/constants')
+  let local = {}
+  //const args = require('./config/args')
+  //const paths = require('./config/paths')
+  //const watch = require('./config/watch')
+  //const constants = require('./config/constants')
+  return local
 };
