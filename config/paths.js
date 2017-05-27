99b5a62c2c32e22a0596bada4a91b162a5063fcf
Matthew Bishop <mattbis@users.noreply.github.com>
2017-05-27 02:19:20 +0100

    Update paths.js

--- config/paths.js
+++ config/paths.js
@@ -3,6 +3,7 @@ const _root = path.resolve(__dirname + '/../')
 let paths = {
   root: _root,
   api: _root+'/api',
-  tmp: _root+'/tmp'
+  tmp: _root+'/tmp',
+  logs: _root+'/logs'
 }
 module.exports = paths
