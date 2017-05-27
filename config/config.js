b4c6af0f84fb928f3025c6c7ee08dcab11562f31
Matthew Bishop <mattbis@users.noreply.github.com>
2017-05-27 02:22:31 +0100

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = function() {
+//const args = require('./config/args')
+//const paths = require('./config/paths')
+//const watch = require('./config/watch')
+//const constants = require('./config/constants')
+};
